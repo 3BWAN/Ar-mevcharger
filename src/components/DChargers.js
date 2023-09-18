7e93257eb11b2cbb39037e66aef776d47f42e14a
3BWAN <109908750+3BWAN@users.noreply.github.com>
2023-09-18 19:16:47 +0400

    Update DChargers.js

--- src/components/DChargers.js
+++ src/components/DChargers.js
@@ -47,7 +47,7 @@ import {
 
 // filter products by function making them only chargers
 const Achargers = products.filter((product) =>
-  product.function === 'charger' && product.id <100 && product.id >200 
+  product.function === 'charger' && product.id ==='22'
 );
     return (
         <Container maxW="5xl" p={{ base: 5, md: 10 }} dir='rtl' alignItems= 'flex-start' > 
